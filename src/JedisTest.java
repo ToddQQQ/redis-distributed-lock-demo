a1179e0cc8800510253e0825e1427684a0ddca2d
ToddQQQ <104179591+ToddQQQ@users.noreply.github.com>
2025-09-10 13:49:09 +0800

    Update JedisTest.java

--- src/JedisTest.java
+++ src/JedisTest.java
@@ -1,12 +1,11 @@
 import redis.clients.jedis.Jedis;
 public class JedisTest {
     public static void main(String[] args) {
-        // 连接本地 Redis（默认端口 6379）
+        // run redis
+        // connect local Redis（inital port 6379）
         try (Jedis jedis = new Jedis("localhost", 6379)) {
-            // 写入一个键值对
             jedis.set("foo", "bar");
-
-            // 取出来看看
+            
             String value = jedis.get("foo");
             System.out.println("Redis 中 foo = " + value);
         }
